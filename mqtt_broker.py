aac3ddb27a8a8970534edc1a040b3755397841c4
TAKDANAI JIRAWANICHKUL <takdanai.3473@mail.kmutt.ac.th>
2019-02-18 23:10:35 +0700

    workV1

--- mqtt_broker.py
+++ mqtt_broker.py
@@ -8,40 +8,52 @@ addr = ('127.0.0.1', SERV_PORT) # Socket address
 s = socket(AF_INET, SOCK_DGRAM) # Create UDP socket
 s.bind(addr)                    # Bind socket to address
 
+subscribe_lst = [] #Subscribe Array
 
-subscribe_lst = []
-
-
-print ('MQTT Broker started ...(UDP)')
+print('---------------------------------------------------------')
+print('|\t\t\tMQTT Broker\t\t\t|')
+print('|\t\t     Group Ubiquitous\t\t\t|')
+print('---------------------------------------------------------')
 
 while(1):
-  print ('>', end = '')
-  txtin,addr = s.recvfrom(MAX_BUF)  # txtin stores receive text
-  ip, port = str(addr[0]), str(addr[1]) 
-  split_txtin = txtin.decode('utf-8').split(':')
-
-  if ( split_txtin[0] == "publish" and len(split_txtin) == 3): # Pulish | Topic | Data
-    topic,data = split_txtin[1],split_txtin[2]
-    print ('Publish from ' + ip + ':' + port + '\tTopic:' + topic +' Data:' + data)
-   
-    # Doing some thing with text
-    txtout = txtin.upper()     # Change text to upper case
-
-    # Send back to client we need to keep subscribe client to send 
-    s.sendto(txtout, addr)     # Send it back to the client
-
-  
-  elif ( split_txtin[0] == "subscribe" and len(split_txtin) == 2): # subscribe | Topic 
-    print("SUB")
-    subscribe_lst.append(ip +':'+ port + ',' + split_txtin[1]) # Make array list of subscribe with topic
-    print(ip +':'+ port + 'add to array list with topic:' + split_txtin[1])
-
-    print ('Subscribe list : ' + str(subscribe_lst))
-
-  else:
-    print("Error : received message error ( Wrong argument or something) ")
-
-
-
+	print ('>', end = '')
+	txtin,addr = s.recvfrom(MAX_BUF)  # txtin stores receive text
 	
+	ip, port = str(addr[0]), str(addr[1]) 
+	split_txtin = txtin.decode('utf-8').split(':')
+
+	# Pubish > constraints  { publih , topic , data }
+	if ( split_txtin[0] == "publish" and len(split_txtin) == 3): 
+		
+		topic,data = split_txtin[1],split_txtin[2]
+		print ('Publish from ' + ip + ':' + port + '\tTopic:' + topic +' Data:' + data)
+
+		# Send message to subscribe client 
+		# In each array of subscribe_lst will keep in style
+		# >>   127.0.0.1:50000,topicname <<< 
+		for x in subscribe_lst:	
+			split_x = x.split(',')  # We need to split IP:Port and topicname
+			if split_x[1] == topic: # Check each item in list have same topic 
+				split_ip_port = split_x[0].split(':') # If found split IP and port
+				addr_send = (split_ip_port[0], int(split_ip_port[1])) # Write in addr_send 
+				print ('   Publish data to ' + split_x[0])
+				s.sendto(data.encode('utf-8'), addr_send) #Send data to client by using addr_send
+
+	# Subscribe > constraints  { subscribe , topic }
+	elif ( split_txtin[0] == "subscribe" and len(split_txtin) == 2): 
+		subscribe_lst.append(ip +':'+ port + ',' + split_txtin[1])  # Make array list of subscribe
+		print('Subscribe ' + ip +':'+ port + ' to list with topic:' + split_txtin[1])
+		print ('\tSubscribe list: ' + str(subscribe_lst))
+
+	# Un-Subscribe > constraints  { unsubscribe , topic }
+	elif (split_txtin[0] == "unsubscribe" and len(split_txtin) == 2):
+		string = ip + ':' + port + ',' + split_txtin[1]
+		subscribe_lst.remove(string) # Remove subscribe client in list 
+		print ('Unsubscribe' + ip + ':' + port + ' from list with topic: ' + split_txtin[1])
+		print ('\tSubscribe list: ' + str(subscribe_lst))	
+
+	# Error 
+	else:
+		print("Error : received message error ( Wrong argument or something) ")
+
 s.close()
--- mqtt_client.py
+++ mqtt_client.py
@@ -1,63 +1,76 @@
+from threading import Thread
 from socket import * 
 import sys
+import time
+import os
 
 MAX_BUF = 2048
 SERV_PORT = 50000
 
-#addr = ('127.0.0.1', SERV_PORT)     # Server socket address 
-s = socket(AF_INET, SOCK_DGRAM)  # Create UDP socket
+def handle_client(s):
+  while True:
+     txtin = s.recv(2048)
+     print ('Message> %s' %(txtin).decode('utf-8')) 
+  return
 
-print('---------------------------------------------------------')
-print('|\t\t\tMQTT Client\t\t\t|')
-print('|\t\t     Group Ubiquitous\t\t\t|')
-print('|\t   In this program will provide sub/pub\t\t|')
-print('|  Subscriber: subscribe broker_ipaddress topicname\t|')
-print('|  Publisher: publish broker_ipaddress topicname data\t|')
-print('---------------------------------------------------------')
+def main():
+	#addr = ('127.0.0.1', SERV_PORT)     # Server socket address 
+	s = socket(AF_INET, SOCK_DGRAM)  # Create UDP socket
+	print('---------------------------------------------------------')
+	print('|\t\t\tMQTT Client\t\t\t|')
+	print('|\t\t     Group Ubiquitous\t\t\t|')
+	print('|\t   In this program will provide sub/pub\t\t|')
+	print('|  Subscriber: subscribe broker_ipaddress topicname\t|')
+	print('|  Publisher: publish broker_ipaddress topicname data\t|')
+	print('---------------------------------------------------------')
+	while(1):
+		print('> ',end='') # Print the prompt
+		sys.stdout.flush()
+		txtout =  sys.stdin.readline().strip() # Take input from user keyboard
+		split_txtout = txtout.split()
 
-# userInput = input()
-# split_userInput = userInput.split()
-# if split_userInput[0] == "subscribe":
-#     print("subscribe mode")
-# elif split_userInput[0] == "publish":
-#     print("publish mode")
-# else:
-#     print("Try again")
-txtout = "home"
-while(1):
-    print('> ',end='') # Print the prompt
-    sys.stdout.flush()
-    txtout =  sys.stdin.readline().strip() # Take input from user keyboard
+		# Pubish > constraints  { publih , topic , data }			
+		if (split_txtout[0] == "publish" and len(split_txtout) == 4): 
+			broker_ipaddress,topicname,data = split_txtout[1],split_txtout[2],split_txtout[3]  
+			addr = (broker_ipaddress, SERV_PORT) # IP with port 
+			print('Publish to IP:' + broker_ipaddress + ' Topic:' + topicname + ' Data:' + data)
+			
+			# Make string and send to broker 
+			data_Send = 'publish:' + topicname + ':' + data
+			s.sendto(data_Send.encode('utf-8'), addr)	
 
-    split_txtout = txtout.split()
-    if (split_txtout[0] == "subscribe" and len(split_txtout) == 3): # Subscribe Mode 
-    	print("Sub")
-    	broker_ipaddress = split_txtout[1]  
-    	topicname = split_txtout[2] 
+		# Subscribe > constraints  { subscribe , topic }
+		elif (split_txtout[0] == "subscribe" and len(split_txtout) == 3): # Subscribe Mode
+			broker_ipaddress,topicname = split_txtout[1],split_txtout[2] 
+			addr = (broker_ipaddress, SERV_PORT) # IP with port 
+			print('Subscribe to IP:' + broker_ipaddress + '. Topic:' + topicname)
+			
+			# Make string and send to broker  
+			data_Send = 'subscribe:' + topicname
+			s.sendto(data_Send.encode('utf-8'), addr) # Convert to byte type and send
 
-    	addr = (broker_ipaddress, SERV_PORT) # IP with port 
+			# Subscribe TOPIC from Broker
+			Thread(target=handle_client, args=(s,)).start()
+			while True:
+				try:
+					time.sleep(0.01)
+				except KeyboardInterrupt: # When user want to unscribe this topic
+					data_Send = 'unsubscribe:' + topicname
+					s.sendto(data_Send.encode('utf-8'), addr) # Convert to byte type and send
+					print ('Stop subscribe topic:' + topicname)
+					Thread(target=handle_client, args=(s,))._stop()
+					break
+		# Error 
+		else:
+			print("Wrong syntax. Please try agian")
+	s.close()
 
-    	print('IP:' + broker_ipaddress + ' Topic:' + topicname)
-    	data_Send = 'subscribe:' + topicname
-    	s.sendto(data_Send.encode('utf-8'), addr)
-    
-    elif (split_txtout[0] == "publish" and len(split_txtout) == 4): # Publish Mode 
-    	broker_ipaddress = split_txtout[1]  
-    	topicname = split_txtout[2] 
-    	data = split_txtout[3] 
-
-    	addr = (broker_ipaddress, SERV_PORT) # IP with port 
-
-    	print('IP:' + broker_ipaddress + ' Topic:' + topicname + ' Data:' + data)
-    	data_Send = 'publish:' + topicname + ':' + data
-    	s.sendto(data_Send.encode('utf-8'), addr)	# Convert to byte type and send
-    	
-    	# modifiedMsg, srvAddr = s.recvfrom(2048)
-    	# print(modifiedMsg.decode('utf-8'))
-    else:
-    	print("Wrong syntax. Please try agian")
-
-    if txtout == 'quit':
-        break
-
-s.close()
+if __name__ == '__main__':
+   try:
+     main()
+   except KeyboardInterrupt:
+     print ('Interrupted ..')
+     try:
+       sys.exit(0)
+     except SystemExit:
+       os._exit(0)
